323cf45061354aae6d1be5a4292820cf45dc2d7a
Mohamed Ali <mohamedali21>
2017-06-10 22:58:14 +0200

    about activity

--- app/src/main/java/com/ourproject/learningapp/activities/About.java
+++ app/src/main/java/com/ourproject/learningapp/activities/About.java
@@ -1,43 +1,32 @@
-package com.ourproject.learningapp.fragments;
+package com.ourproject.learningapp.activities;
 
-import android.app.Dialog;
-import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ourproject.learningapp.R;
-import com.ourproject.learningapp.activities.MainActivity;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Created by Mohamed Ali on 6/9/2017.
- */
-public class AboutFrag extends Fragment {
+public class About extends AppCompatActivity {
     private TextView textView;
     private StringBuilder text = new StringBuilder();
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_about, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
 
-
-        textView = (TextView) view.findViewById(R.id.about);
+        textView = (TextView) findViewById(R.id.about);
         BufferedReader reader = null;
 
         try {
             reader = new BufferedReader(
-                    new InputStreamReader(getActivity().getAssets().open("file.txt")));
+                    new InputStreamReader(getAssets().open("file.txt")));
 
             // do reading, usually loop until end of file reading
             String mLine;
@@ -46,7 +35,7 @@ public class AboutFrag extends Fragment {
                 text.append('\n');
             }
         } catch (IOException e) {
-            Toast.makeText(getActivity(), "Error reading file!", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Error reading file!", Toast.LENGTH_LONG).show();
             e.printStackTrace();
         } finally {
             if (reader != null) {
@@ -59,9 +48,6 @@ public class AboutFrag extends Fragment {
             textView.setTextDirection(View.TEXT_DIRECTION_FIRST_STRONG_RTL);
             textView.setText(text);
             textView.setTypeface(MainActivity.font);
-
-
-            return view;
 
         }
     }
